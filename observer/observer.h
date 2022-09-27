25a1eaeb2734e5d6b15e894409238761d8eee16e
Gyubeom Han <sy0871@naver.com>
2022-09-27 16:47:59 +0900

    Use observer doubly linked list

--- observer/observer.h
+++ observer/observer.h
@@ -6,11 +6,12 @@ class Observer {
 	friend class Subject;
 
 public:
-	Observer() : m_next(nullptr) {}
+	Observer() : m_next(nullptr), m_prev(nullptr) {}
 	virtual ~Observer() {}
 
 	virtual void OnNotify(const Entity& entity, Event event) = 0;
 
 private:
 	Observer* m_next;
+	Observer* m_prev;
 };
--- observer/subject.h
+++ observer/subject.h
@@ -3,32 +3,29 @@
 #include "observer.h"
 
 class Subject {
-
 public:
-	Subject() : m_head(nullptr) {}
+	Subject() : m_head(nullptr) { }
 
 	void AddObserver(Observer* observer) {
 		observer->m_next = m_head;
+		if (m_head != nullptr) m_head->m_prev = observer;
+		observer->m_prev = nullptr;
 		m_head = observer;
 	}
 	void RemoveObserver(Observer* observer) {
 		if (m_head == observer) {
 			m_head = observer->m_next;
-			observer->m_next = nullptr;
-			return;
 		}
-
-		auto current = m_head;
-		while (current != nullptr) {
-			if (current->m_next == observer) {
-				current->m_next = observer->m_next;
-				observer->m_next = nullptr;
-				return;
-			}
-			current = current->m_next;
+		if (observer->m_next != nullptr) {
+			observer->m_next->m_prev = observer->m_prev;
 		}
+		if (observer->m_prev != nullptr) {
+			observer->m_prev->m_next = observer->m_next;
+		}
+		observer->m_next = nullptr;
+		observer->m_prev = nullptr;
 	}
-	
+
 protected:
 	void Notify(const Entity& entity, Event event) {
 		auto observer = m_head;
@@ -40,4 +37,5 @@ protected:
 
 private:
 	Observer* m_head;
+
 };
