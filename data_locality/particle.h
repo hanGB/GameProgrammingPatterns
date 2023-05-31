5a09dafb940ee6d5d2ef2ddb2bab7036b7030833
Gyubeom Han <sy0871@naver.com>
2023-05-31 15:38:49 +0900

    Particle system without active check(do not use if)

--- data_locality/particle.h
+++ data_locality/particle.h
@@ -10,14 +10,6 @@ public:
 		m_elapsedTime += time;
 	}
 
-	bool GetIsActive() const
-	{
-		return m_isActive;
-	}
-
-	void SetIsActive(bool active) {
-		m_isActive = active;
-	}
 	void SetLocation(double x, double y)
 	{
 		m_x = x;
@@ -25,8 +17,6 @@ public:
 	}
 
 private:
-	bool m_isActive = false;
-
 	double m_x = 1, m_y = 0.0;
 	double m_speed = 0.0;
 	double m_elapsedTime = 0.0;
@@ -34,25 +24,20 @@ private:
 
 class ParticleSystem {
 public:
-	ParticleSystem() : m_numParticles(0) 
+	ParticleSystem() : m_numParticles(0), m_numActive(0)
 	{
 
 	}
 
 	void Update(double time)
 	{
-		for (int i = 0; i < m_numParticles; ++i) {
-			
-			if (m_particles[i].GetIsActive()) {
-				//std::cout << i << "번 - 활성화\n";
-				m_particles[i].Update(time);
-			}
+		for (int i = 0; i < m_numActive; ++i) {
+			m_particles[i].Update(time);
 		}
 	
 	}
 	void AddParticle(double x, double y) {
 		if (c_MAX_PARTICLE > m_numParticles) {
-			m_particles[m_numParticles].SetIsActive(false);
 			m_particles[m_numParticles].SetLocation(x, y);
 			m_numParticles++;
 		}
@@ -61,12 +46,31 @@ public:
 		int start = m_numParticles % 4;
 		int gap = m_numParticles % 3 + 2;
 		for (int i = start; i < m_numParticles; i += gap) {
-			m_particles[i].SetIsActive(true);
+			ActiveParticle(i);
 		}
 		start++;
 		gap--;
 		for (int i = start; i < m_numParticles; i += gap) {
-			m_particles[i].SetIsActive(false);
+			DeactivateParticle(i);
+		}
+	}
+
+	void ActiveParticle(int index) {
+		if (index >= m_numActive) {
+			Particle temp = m_particles[m_numActive];
+			m_particles[m_numActive] = m_particles[index];
+			m_particles[index] = temp;
+
+			m_numActive++;
+		}
+	}
+	void DeactivateParticle(int index) {
+		if (index < m_numActive) {
+			Particle temp = m_particles[m_numActive];
+			m_particles[m_numActive] = m_particles[index];
+			m_particles[index] = temp;
+
+			m_numActive--;
 		}
 	}
 
@@ -74,5 +78,6 @@ private:
 	static const int c_MAX_PARTICLE = 1'000'000;
 
 	int m_numParticles;
+	int m_numActive;
 	Particle m_particles[c_MAX_PARTICLE];
 };
